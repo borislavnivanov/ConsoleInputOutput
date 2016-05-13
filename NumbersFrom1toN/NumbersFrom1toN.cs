919dc0ac8a26553eb433759fadf48baeb005e354
borislavnivanov <borislavnivanov@gmail.com>
2016-05-14 00:26:23 +0300

    New projects 7 and 8

---
+++ NumbersFrom1toN/NumbersFrom1toN.cs
@@ -0,0 +1,32 @@
+﻿/*Write a program that reads an integer number n from the console and prints all the numbers in the interval [1..n], each on a single line.
+Note: You may need to use a for-loop.
+
+Examples:
+
+input	output
+3      	1
+        2
+        3
+5      	1
+        2
+        3
+        4
+        5
+1	    1
+*/
+
+using System;
+
+
+class NumbersFrom1toN
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter the number up to which to count");
+        int countLimit = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= countLimit; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ SumOf5Numbers/SumOf5Numbers.cs
@@ -0,0 +1,28 @@
+﻿/*Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
+Examples:
+
+numbers	sum
+1 2 3 4 5	        15
+10 10 10 10 10  	50
+1.5 3.14 8.2 -1 0	11.84
+*/
+
+using System;
+
+
+class SumOf5Numbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter five numbers:");
+        string[] userInput = Console.ReadLine().Split();
+        double a = double.Parse(userInput[0]);
+        double b = double.Parse(userInput[1]);
+        double c = double.Parse(userInput[2]);
+        double d = double.Parse(userInput[3]);
+        double e = double.Parse(userInput[4]);
+
+        Console.WriteLine("The sum os the numbers is : {0}.", (a + b + c + d + e));
+
+    }
+}
