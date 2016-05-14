e3034caf634cc314bda4210e8af5c8662eca79ef
borislavnivanov <borislavnivanov@gmail.com>
2016-05-15 01:46:56 +0300

    Update

---
+++ FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,46 @@
+﻿/*Write a program that reads a number n and prints on the console the first n members of the Fibonacci sequence 
+(at a single line, separated by comma and space - ,) : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ….
+Note: You may need to learn how to use loops.
+
+
+Examples:
+
+n	comments
+1	0
+3	0 1 1
+10	0 1 1 2 3 5 8 13 21 34
+*/
+
+
+using System;
+using System.Numerics;
+
+
+class FibonacciNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter the amount of memebers to show");
+        int lenght = int.Parse(Console.ReadLine());
+
+        BigInteger firstnumber = 0;
+        BigInteger secondnumber = 1;
+        BigInteger currentnumber;
+
+        Console.Write(firstnumber);
+        if (lenght > 2)
+        {
+            Console.Write(", {0}", secondnumber);
+        }
+
+        for (int i = 2; i < lenght; i++)
+        {
+            currentnumber = firstnumber + secondnumber;
+            Console.Write(", {0}", currentnumber);
+            firstnumber = secondnumber;
+            secondnumber = currentnumber;
+
+        }
+        Console.WriteLine();
+    }
+}
---
+++ SumOfNNumbers/SumOfNNumbers.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Write a program that enters a number n and after that enters more n numbers and calculates and prints their sum. 
+ * Note: You may need to use a for-loop.
+ * Examples:
+
+numbers	sum
+3	    90
+20	
+60	
+10	
+5	    6.5
+2	
+-1	
+-0.5	
+4	
+2	
+1	    1
+1	
+ */
+
+using System;
+
+
+class SumOfNNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter the count of N numbers");
+        int count = int.Parse(Console.ReadLine());
+
+        double sum = 0;
+
+        Console.WriteLine("Please enter number to be calculated");
+
+
+        for (int i = 1; i <= count; i++)
+        {
+            double number = double.Parse(Console.ReadLine());
+            sum += number;
+        }
+
+        Console.WriteLine("The sum of the numbers is {0}.", sum);
+    }
+}
